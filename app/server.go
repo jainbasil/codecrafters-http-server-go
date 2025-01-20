b4cdecf3f1b7766f25b1b964ef9f499ddffaa1e2
Jain Basil Aliyas <jainbasil@gmail.com>
2025-01-20 18:51:50 +0530

    respond with 200

--- app/server.go
+++ app/server.go
@@ -1,26 +1,49 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	fmt.Printf("Listening on host: localhost, port: 4221\n")
 
 	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer func(l net.Listener) {
+		err := l.Close()
+		if err != nil {
+			log.Fatal("Error closing the listener")
+		}
+	}(l)
+
+	for {
+		conn, err := l.Accept()
+		log.Println("Connection received")
+		if err != nil {
+			log.Fatal("Error accepting connection: ", err.Error())
+		}
+
+		// handle the connection
+		go func(conn net.Conn) {
+			buf := make([]byte, 1024)
+			_, err := conn.Read(buf)
+			if err != nil {
+				fmt.Println("Error reading: ", err.Error())
+			}
+			fmt.Println(bytes.NewBuffer(buf).String())
+
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+			conn.Close()
+		}(conn)
 	}
 }
